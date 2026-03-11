4d596c02b05d97430b96008826f005711a9c42fb
nejcflegar <nejc.flegar.si@gmail.com>
2026-03-11 20:39:43 +0100

    Andrej type-a commit

---
+++ nal4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "Header.h"
+
+
+int main(){
+
+    for(; kolikokrat != -1; kolikokrat--){
+        helloh();
+    }
+    return 0;
+}
+
+
---
+++ nal5.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ 
+int kolikokrat = 10;
+
+void helloh(){
+    printf("helloh%d\n",kolikokrat);
+}
